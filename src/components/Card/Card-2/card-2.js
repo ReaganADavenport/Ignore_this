88a23c656905f2133bf1204a0e749119a4ad65b9
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2020-07-29 09:52:33 -0400

    added react-flip-card to card 2 + css

--- src/components/Card/Card-2/card-2.js
+++ src/components/Card/Card-2/card-2.js
@@ -1,29 +1,42 @@
 import React, {Component} from 'react';
+import ReactCardFlip from 'react-card-flip';
 import logo from "../../../images/controller.png"
 
 import "./card-2.css";
 
-const PressA = () => {
-    return(
-        <div className="flip-pcard">
-            <div className="flip-pcard-inner">
-                <div className="flip-pcard-front">
+class PressA extends Component {
+    constructor() {
+        super();
+            this.state = {
+                isFlipped: false
+            };
+
+            this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+    }
+
+    render(){
+        return(
+            <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
+                <div className="flip-pcard-front" onClick={this.handleClick}>
                     <img src={logo}/>
                     <h4>Press A to Begin</h4>
                     <p>Using the vanacular of gaming in youth ministry</p>
                 </div>
 
-                <div className="flip-pcard-back">
+                <div className="flip-pcard-back" onClick={this.handleClick}>
                     <img src={logo}/>
                     <p>React, HTML, CSS, JavaScipt, API</p>
                     <p><a href="http://www.pressatobegin.com/">PressAToBegin.com</a> </p>
                     <p><a href="https://github.com/ReaganADavenport/press-A-to-begin">GitHub Repository</a> </p>
                 </div>
-
-            </div>
-
-        </div>
-    )
+            </ReactCardFlip>
+        )
+    }
 }
 
 
