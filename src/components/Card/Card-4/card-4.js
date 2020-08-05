d77e8d1113eb235479b35a6d93eaf3701be28f58
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2020-08-05 11:13:17 -0400

    made the travelling tails card into the DRC candle card

--- src/components/Card/Card-4/card-4.js
+++ src/components/Card/Card-4/card-4.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import ReactCardFlip from 'react-card-flip';
-import logo from "../../../images/dog.png"
+import logo from "../../../images/candle.png"
 
 import "./card-4.css";
 
-class Dog extends Component {
+class Candle extends Component {
     constructor() {
         super();
             this.state = {
@@ -22,18 +22,17 @@ class Dog extends Component {
     render(){
         return(
             <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
-                        <div className="flip-dcard-front" onClick={this.handleClick}>
+                        <div className="flip-ccard-front" onClick={this.handleClick}>
                             <img src={logo}/>
-                            <h5>Traveling Tails</h5>
-                            <p>Use Google Maps to find pet-friendly hotels, restaurants, parks, vets, and pet-supply stores.</p>
+                            <h5>Candle Ministry</h5>
+                            <p>Use e-commerce to purchase beeswax candles from a Greek Orthodox retreat center</p>
                         </div>
 
-                        <div className="flip-dcard-back" onClick={this.handleClick}>
+                        <div className="flip-ccard-back" onClick={this.handleClick}>
                             <>
                                 <img src={logo}/>
-                                <p>HTML, CSS, JavaScript, Google Maps API</p>
-                                <p>Demo Video</p>
-                                <p><a href="https://github.com/ReaganADavenport/Traveling-Tails-2">GitHub Repository</a> </p>
+                                <p>WordPress, e-commerce, UPS API, Plug-ins</p> /*Insanity management */
+                                <p><a href="https://thediakoniaretreatcenter.com/candle-ministry-2/">thediakoniaretreatcenter.com</a> </p>
                             </>
                         </div>
             </ReactCardFlip>
@@ -42,4 +41,4 @@ class Dog extends Component {
 }
 
 
-export default Dog; 
+export default Candle; 
