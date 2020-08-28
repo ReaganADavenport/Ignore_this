d3a15d447b3e80a7d14ad8deb1c02cd914de77f6
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2020-08-28 09:43:52 -0400

    fixed spelling error on about me page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -9,7 +9,7 @@ const About = () => {
             <h1>About Me</h1>
             <div className="Content">
                 <p>Hi! My name is Reagan Davenport.</p>
-                <p>I'm a full-stack developer. I build, and occationally design, beautiful, responsive, and functional websites to facilitate e-commerce.</p>
+                <p>I'm a full-stack developer. I build, and occasionally design, beautiful, responsive, and functional websites to facilitate e-commerce.</p>
                 <p>I prefer to work in a full-cycle, from mock-ups to HTML/CSS/JavaScript.</p>
                 <p>I've only been working in the field for a year, but I'm always on the hunt for new things to learn.</p>
             </div>
